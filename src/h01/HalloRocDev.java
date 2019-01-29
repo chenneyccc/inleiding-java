110b684af4a710d16867e319a92281e9109e6a16
chenneyccc <47109662+chenneyccc@users.noreply.github.com>
2019-01-29 10:01:10 +0100

    Update HalloRocDev.java

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo beste mensen");
+        System.out.println("Hallo 123");
     }
 }
