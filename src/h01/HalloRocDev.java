f31a2451e1f468c7c59d9f28516279d499836216
chenneyccc <chenneycc@outlook.com>
2019-01-29 10:10:56 +0100

    3.0

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo beste wyuiewka");
+        System.out.println("Hallo beste jamaaka");
     }
 }
